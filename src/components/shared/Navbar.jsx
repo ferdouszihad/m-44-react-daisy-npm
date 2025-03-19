4171a308691022466dc3545a81dfbda65718bc38
ferdouszihad <f.zihad333@gmail.com>
2025-03-19 23:03:19 +0600

    nav toggle animation with state

--- src/components/shared/Navbar.jsx
+++ src/components/shared/Navbar.jsx
@@ -1,6 +1,8 @@
 import { BsMenuApp, BsMenuButton } from "react-icons/bs";
 import Logo from "./Logo";
-import { BiMenu } from "react-icons/bi";
+import { BiCross, BiMenu } from "react-icons/bi";
+import { useState } from "react";
+import { ImCross } from "react-icons/im";
 
 const Navbar = () => {
   const routes = [
@@ -9,6 +11,8 @@ const Navbar = () => {
     { name: "Services", path: "/services" },
     { name: "Contact", path: "/contact" },
   ];
+
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
   return (
     <nav className="px-4 flex justify-between items-center py-3 border-b-2 border-base-300 bg-yellow-50">
       <Logo></Logo>
@@ -23,7 +27,27 @@ const Navbar = () => {
       </div>
 
       <div className="mobile-opt block lg:hidden">
-        <BiMenu size={32} className="cursor-pointer"></BiMenu>
+        <BiMenu
+          onClick={() => setIsMenuOpen(true)}
+          size={32}
+          className={`cursor-pointer ${isMenuOpen ? "hidden" : "block"}`}
+        ></BiMenu>
+        <ImCross
+          onClick={() => setIsMenuOpen(false)}
+          size={24}
+          className={`cursor-pointer ${isMenuOpen ? "block" : "hidden"}`}
+        ></ImCross>
+        <ul
+          className={`space-y-3 absolute left-0 bg-yellow-50 w-full p-4 ${
+            isMenuOpen ? "top-16" : "-top-[9999px]"
+          } `}
+        >
+          {routes.map((route) => (
+            <li className="border-b-2 border-base-300" key={route.path}>
+              <a href={route.path}>{route.name}</a>
+            </li>
+          ))}
+        </ul>
       </div>
     </nav>
   );
