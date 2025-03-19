48a4d0478a13766e82d12f5d1858af9e66be01e2
ferdouszihad <f.zihad333@gmail.com>
2025-03-19 22:28:15 +0600

    1- install daisy-ui

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <h1 className="py-5 text-5xl text-center">
+        Yahoo 😀 !! Daisy Khala Came to our life again
+      </h1>
+    </>
+  );
+}
+
+export default App;
